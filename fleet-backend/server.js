e7ee242b4e2b575373c4928a68bdbe359a9650b2
Islem-Boubaker <islemboubaker93@gmail.com>
2026-02-10 00:05:21 +0100

    Add auth middleware and user model

---
+++ fleet-backend/server.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.use(cors());
+app.use(bodyParser.json());
+
+
+
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
