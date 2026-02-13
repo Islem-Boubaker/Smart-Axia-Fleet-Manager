903abcc59d3181b0876cdf0bd38c11b7d111eea3
Islem-Boubaker <islemboubaker93@gmail.com>
2026-02-13 18:49:55 +0100

    Add vehicle management: implement vehicle model, service, controller, and routes

---
+++ fleet-backend/services/vehicle.service.js
@@ -0,0 +1,26 @@
+import Vehicle from '../models/vehicle.model.js';
+
+export const createVehicle = async (data) => {
+    return await Vehicle.create(data);
+};
+
+export const getAllVehicles = async () => {
+    return await Vehicle.findAll();
+};
+
+export const getVehicleById = async (id) => {
+    return await Vehicle.findByPk(id);
+};
+
+export const updateVehicle = async (id, data) => {
+    const vehicle = await Vehicle.findByPk(id);
+    if (!vehicle) return null;
+    return await vehicle.update(data);
+};
+
+export const deleteVehicle = async (id) => {
+    const vehicle = await Vehicle.findByPk(id);
+    if (!vehicle) return null;
+    await vehicle.destroy();
+    return true;
+};
